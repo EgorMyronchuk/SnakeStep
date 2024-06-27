287a8b12aab96932279962c631d324a2eb1099a5
egormyronchuk <egormyronchuk@gmail.com>
2024-06-27 20:53:50 +0300

    20 53 push

---
+++ src/main/java/snake/client/Decoder.java
@@ -0,0 +1,20 @@
+package snake.client;
+
+import com.codenjoy.dojo.services.Direction;
+import com.codenjoy.dojo.services.Point;
+
+import java.util.List;
+
+public class Decoder {
+    static List<Point> Path;
+
+    public Direction getDirection() {
+        Point stepPath = Path.getLast();
+        Path.removeLast();
+        switch ()
+    }
+    public int twoPointToDirection (Point curr , Point next) {
+
+    }
+
+}
